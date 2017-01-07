6819c3ad64f4452d046a6f94108c6ed144fad644
Mingyang Li <tslmy@outlook.com>
2017-01-07 15:46:38 +0800

    Enhance math.js
    Ported setFromEuler and setFromAxisAngle from THREE.js

--- 3Dmol/WebGL/math.js
+++ 3Dmol/WebGL/math.js
@@ -113,7 +113,51 @@ $3Dmol.Quaternion.prototype = {
         this.z = qaz * qbw + qaw * qbz + qax * qby - qay * qbx;
         this.w = qaw * qbw - qax * qbx - qay * qby - qaz * qbz;
 
+    },
+
+    setFromEuler: function ( euler ) {
+        //Adoped from THREE.js/math/Quaternion.js
+
+        // http://www.mathworks.com/matlabcentral/fileexchange/
+        //  20696-function-to-convert-between-dcm-euler-angles-quaternions-and-euler-vectors/
+        //  content/SpinCalc.m
+
+        var c1 = Math.cos( euler.x / 2 );
+        var c2 = Math.cos( euler.y / 2 );
+        var c3 = Math.cos( euler.z / 2 );
+        var s1 = Math.sin( euler.x / 2 );
+        var s2 = Math.sin( euler.y / 2 );
+        var s3 = Math.sin( euler.z / 2 );
+
+        //var order = euler.order;
+
+        //} else if ( order === 'YXZ' ) {
+        this.x = s1 * c2 * c3 + c1 * s2 * s3;
+        this.y = c1 * s2 * c3 - s1 * c2 * s3;
+        this.z = c1 * c2 * s3 - s1 * s2 * c3;
+        this.w = c1 * c2 * c3 + s1 * s2 * s3;
+        return this;
+
+    },
+
+
+    setFromAxisAngle: function ( axis, angle ) {
+        //Adoped from THREE.js/math/Quaternion.js
+
+        // http://www.euclideanspace.com/maths/geometry/rotations/conversions/angleToQuaternion/index.htm
+
+        // assumes axis is normalized
+
+        var halfAngle = angle / 2, s = Math.sin( halfAngle );
+
+        this.x = axis.x * s;
+        this.y = axis.y * s;
+        this.z = axis.z * s;
+        this.w = Math.cos( halfAngle );
+
+        return this;
     }
+
 };
 
 //A 2 Vector
